09d973abc4e3626ba3a30769f0c7a870bfa3e97c
SkyLegends528 <vitorperacini528@gmail.com>
2024-09-17 17:47:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const avanca = document.querySelectorAll('.btn-proximo');
+avanca.forEach(button => {
+    button.AddEventListner('click', function(){
+const atual = document.querySelector('.ativo');
+const proximopasso = 'passo-' +this.getAttribute('data-proximo');
+atual.classList.remove('.ativo');
+document.getElementById(proximoPasso).classList.add('.ativo');
+    })
+})
